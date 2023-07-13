8ff8ff4e4b55acb8a6a28475a0ec90e5a60f5dc6
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-13 17:09:04 +0300

    learning c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is my starting point
+ *
+ * Return: i will return a 0 obviosly
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
