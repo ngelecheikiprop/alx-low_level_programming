a7c5b3ef58898089e639283d755a36263640008f
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-14 15:41:06 +0300

    learning conditions

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ *
+ * Return: returns(0)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a', letter =< 'z', letter ++)
+	{
+		putchar(letter);
+	}
+}
