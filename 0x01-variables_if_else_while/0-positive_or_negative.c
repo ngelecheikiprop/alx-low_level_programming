eca1e0c67b46ce99fec0b59a18373c6d1356cf7c
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-14 14:48:45 +0300

    learning conditions

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
 	/* your code goes there */
 	if(n>0)
 	{
-		printf("%dis positive\n",n);
+		printf("%d is positive\n",n);
 	}
 	else if(n==0)
 	{
-		printf("%dis zero\n",n);
+		printf("%d is zero\n",n);
 	}
 	else if(n>0)
 	{
