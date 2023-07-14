36ee166c9512bfe00b347888c50cb90fc2dfe9df
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-14 23:07:12 +0300

    learning conditions

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - everything is here
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	/*link*/
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1 + i; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
