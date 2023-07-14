c315f6a17b8f47cfe4c035818620017fa77cfa51
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-14 21:30:27 +0300

    learning conditions

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is a entry point
+ *
+ * Return: always returns (0)
+ */
+
+int main(void)
+{
+	int hex;
+	/*blannk*/
+	for (hex=0; hex < 16, hex++)
+	{
+		if (hex < 10)
+			putchar(hex + '0');
+		else
+			putchar('a' + (hex - 10));
+	}
+	putchar('\n');
+	return (0);
+}
