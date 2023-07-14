816a30316558593d895c5a519c9b0e24731b75ca
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-14 17:35:07 +0300

    learning conditions

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ *
+ * Return: return(0)
+ *
+ */
+int main(void)
+{
+	int lowerCase;
+	int upperCase;
+
+	for (lowerCase = 'a'; lowerCase =< 'z'; lowerCase ++)
+		putchar(lowerCase);
+	for (upperCase = 'A'; upperCase =< 'Z'; upperCase ++)
+		putchar(upperCase);
+	putchar('\n');
+}
