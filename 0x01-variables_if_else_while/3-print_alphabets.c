504bb5c6ea35ea5100c02f0470cd8fd592e26fa6
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-14 17:40:17 +0300

    learning conditions

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,10 @@ int main(void)
 	int lowerCase;
 	int upperCase;
 
-	for (lowerCase = 'a'; lowerCase =< 'z'; lowerCase ++)
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 		putchar(lowerCase);
-	for (upperCase = 'A'; upperCase =< 'Z'; upperCase ++)
+	for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
 		putchar(upperCase);
 	putchar('\n');
+	return (0);
 }
