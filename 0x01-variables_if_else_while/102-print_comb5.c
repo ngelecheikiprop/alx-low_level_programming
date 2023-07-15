da33960fca0979d5dff364f33fe7a56d7229dad4
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-15 05:16:13 +0300

    learning conditions

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point of program
+ *
+ * Return: always(0)
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+	/*blank line*/
+	for (i = 00; i < 100; i ++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			puthcar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
