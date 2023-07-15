7cb4017f2bfba99815436bbad48d48af07adb1e3
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-15 05:17:15 +0300

    learning conditions

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
 			putchar(' ');
-			puthcar((j / 10) + '0');
+			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i < 98)
 			{
