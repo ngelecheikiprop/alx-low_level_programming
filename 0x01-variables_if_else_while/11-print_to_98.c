d6236745f2234d1d0c2eb118f2bd6bc9dcb781d8
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-18 18:19:25 +0300

    learning conditions

---
+++ 0x01-variables_if_else_while/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ *print_to_98 - description
+ *@n: given number
+ *
+ *Return: function of no return
+ */
+void print_to_98(int n)
+{
+	int i;
+	int curr;
+	/*blank line*/
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			if (i < 0)
+			{
+				curr = -i;
+			}
+			else
+			{
+				curr = i;
+			}
+			_putchar(curr / 100 % 10 + '0');
+			_putchar(curr / 10 % 10 + '0');
+			_putchar(curr % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			_putchar(i / 100 % 10 + '0');
+			_putchar(i / 10 % 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
