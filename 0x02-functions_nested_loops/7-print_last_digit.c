377c428bb29c98ecb5e87e8db190bbfaeb925af3
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-18 13:09:59 +0300

    learning nested loops in c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_last_digit - print last digit
+ *@number: given number
+ *
+ *Return: value of last digit
+ */
+int print_last_digit(int number)
+{
+	int remainder;
+	/*blank line*/
+	remainder = number % 10;
+	return (remainder);
+}
