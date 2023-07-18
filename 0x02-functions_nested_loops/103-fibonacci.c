8824eedbd6e8a9ff13cd1c66ad2b59d74055d17e
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-19 01:41:56 +0300

    learning nested loops in c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+/**
+ *main - starting point
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	int fn;
+	int sn;
+	int nn;
+	int i;
+	int sum;
+	/*blank*/
+	fn = 1;
+	sn = 2;
+	sum = 2 + sum;
+	for (i = 3; i <= 50; i++)
+	{
+		nn = fn + sn;
+		if (nn >= 4000000)
+		{
+			break;
+		}
+		if ((nn % 2) == 0)
+			sum = sum + nn;
+		fn = sn;
+		sn = nn;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
