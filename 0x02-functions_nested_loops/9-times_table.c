beac5b7a364b5655215c4a2bfda0679468da3eea
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-18 14:36:39 +0300

    learning nested loops in c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *times_table - print the times 9 table
+ *
+ *Return: function of no return
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int mul;
+	/*blank line*/
+	for (y = 0; y < 10; y++)
+	{
+		_putchar('0');
+		for (x = 0; x < 10; x++)
+		{
+			mul = x * y;
+			if (mul < 10)
+			{
+				_putcahr(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mul + '0');
+			}
+			else
+			{
+				_putcahr(',');
+				_putchar(' ');
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
