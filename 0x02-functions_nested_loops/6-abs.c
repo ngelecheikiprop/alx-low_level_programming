fa2d59972aedcc6b426ff29d5a4f08b79f93a7a4
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-18 12:57:35 +0300

    learning nested loops in c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - findds the absolute of a number
+ *@: give the number
+ *
+ *Return: returns the absolute number
+ */
+int _abs(int)
+{
+	if (int < 0)
+		return (-int);
+	else
+		return (int);
+}
