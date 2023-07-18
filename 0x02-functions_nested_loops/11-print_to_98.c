6c0fd082d783d8043f109785a4e5c04eba344050
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-18 16:09:21 +0300

    learning nested loops in c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,46 +6,17 @@
  */
 void print_to_98(int n)
 {
-	int i;
+	int step;
 	/*blank line*/
-	if (n <= 98)
+	step = (n <= 98) ? 1 : -1;
+	while ( n != 98)
 	{
-		for (i = n; i < 98; i++)
-		{
-			if(i < 0)
-			{
-				_putchar('-');
-				i = -i;
-			}
-			if (i >= 100)
-			{
-			        _putchar(((i / 100) % 10) + '0');
-			}
-			if (i >= 10)
-			{
-				_putchar(((i / 10) % 10) + '0');
-			}
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	else
-	{
-		for (i = n; i > 98; i--)
-		{
-			if (i >= 100)
-			{
-				_putchar(((i / 100) % 10) + '0');
-			}
-			if (i >= 10)
-			{
-				_putchar(((i / 10) % 10) + '0');
-			}
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
+		_putchar(n / 100 % 10 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
+		n += step;
 	}
 	_putchar('9');
 	_putchar('8');
