36e42903015e931bef948ce7c16ce17488b1cae7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-18 12:42:09 +0300

    learning nested loops in c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign - printing signs
+ *@n: number to check
+ *
+ *Return: returns conditionally
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
