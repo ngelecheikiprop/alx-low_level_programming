475556f54524fed05899a700dd4d7ed7b815be5a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-19 00:33:59 +0300

    learning nested loops in c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is the main
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int mul;
+	/*blank line*/
+	mul = 1;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+			mul = mul * i;
+		if((i % 5) == 0)
+			mul = mul * i
+	}
+	printf("%d\n",mul)
+}
