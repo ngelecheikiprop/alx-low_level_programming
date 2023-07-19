a64db0373857b5cd1c412ea1d4b7dbe74d6f2860
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-19 05:56:06 +0300

    learning nested loops in c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 	long double fn;
 	long double sn;
 	long double nn;
-	long double i;
+	int i;
 	/*blank line*/
 	fn = 1;
 	sn = 2;
@@ -17,15 +17,7 @@ int main(void)
 	for (i = 3; i <= 98; i++)
 	{
 		nn = fn + sn;
-		if (i > 92 && i < 98)
-			nn = nn + 1;
-		if (i == 98)
-			nn = nn + 2;
 		printf(", %.0Lf", nn);
-		if (i > 92 && i < 98)
-			nn = nn - 1;
-		if (i == 98)
-			nn = nn -2;
 		fn = sn;
 		sn = nn;
 	}
