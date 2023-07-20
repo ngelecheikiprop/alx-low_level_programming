be4c6690abdac858498549000683dff84df8bcae
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-20 12:49:23 +0300

    learning more on nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_most_numbers -  prints the numbers, from 0 to 9, followed by a new line.
+ *print_most_numbers -  prints the numbers, from 0 to 9.
  *
  *Return: function of no return
  */
@@ -15,5 +15,4 @@ void print_most_numbers(void)
 		_putchar(i + '0');
 	}
 	_putchar('\n');
-	return;
 }
