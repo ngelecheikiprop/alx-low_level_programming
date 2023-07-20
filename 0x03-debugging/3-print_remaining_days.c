9c73b8bf1495200e276aa3a16717c961af233189
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-20 15:59:00 +0300

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,14 +23,17 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && (day == 60 && year % 4))
+		if (month == 2 && (day == 60 && (year % 4)))
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			if ( (year % 4) == 0)
+				printf("Remaining days: %d\n", 366 - day);
+			else
+				printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
