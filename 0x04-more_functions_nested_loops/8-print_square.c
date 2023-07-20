220f190146f9fe35d232b07387d0e927a3092134
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-20 13:25:19 +0300

    learning more on nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *print_square - prints a square, followed by a new line.
-*
+*@size : size of square you want
 *Return: function of no return
 */
 void print_square(int size)
