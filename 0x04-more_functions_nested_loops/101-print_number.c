e6aeeab59f145d6b4fc705ca466a67d6f30369f3
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-21 04:20:25 +0300

    did maths tasks outside putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,28 @@
  */
 void print_number(int n)
 {
+	unsigned int ones, tens, hundreds, thousands;
+
 	if (n < 0)
 	{
 		n = n * -1;
 		_putchar('-');
 	}
 	if (n >= 1000)
-		_putchar((n / 1000) + '0');
+	{
+		thousands = (n / 1000) % 10;
+		_putchar(thousands  + '0');
+	}
 	if (n >= 100)
-		_putchar(((n / 100) % 10) + '0');
+	{
+		hundreds = (n / 100) % 10;
+		_putchar(hundreds  + '0');
+	}
 	if (n >= 10)
-		_putchar(((n / 10) % 10) + '0');
-	_putchar((n % 10) + '0');
+	{
+		tens = (n / 10) % 10;
+		_putchar(tens + '0');
+	}
+	ones = n % 10;
+	_putchar(ones + '0');
 }
