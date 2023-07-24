fabc773c0a7c58907b7e5497d21a9de7e8cfdf23
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-24 02:21:36 -0700

    0. 98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * reset_to_98 - resets number to 98
+ *@n : address of number
  *
  *Return: nothing
  */
 void reset_to_98(int *n)
 {
-  
+	*n = 98;
 }
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+_putchar(char);
+void reset_to_98(int *n);
+
+#endif
