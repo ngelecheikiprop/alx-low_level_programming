4aff6af8a765376506b4c671079686e311701729
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-24 03:07:08 -0700

    i can find youre string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen - counts length of a string.
+ *@s: pointer
+ *
+ *Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	while (*s != '\0')
+	{
+		length++;
+		*(s + 1);
+	}
+	return (length);
+}
