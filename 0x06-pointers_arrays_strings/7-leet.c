59929a6e092a183e72445438ca1f3d780485f774
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-27 20:23:33 +0000

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,25 @@
 #include "main.h"
 /**
   *leet -  encodes a string into 1337.
-  *@ch: - what to encode
+  *@c: - what to encode
   *
   *Return: char
   */
-char *leet(char *ch)
+char *leet(char *c)
 {
 	int i;
-	char *c = ch;
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
 		if (c[i] == 'a' || c[i] == 'A')
 			c[i] = '4';
-		if (c[i] == 'e' || c[i] == 'E')
+		else if(c[i] == 'e' || c[i] == 'E')
 			c[i] = '3';
-		if (c[i] == 'o' || c[i] == 'O')
+		else if (c[i] == 'o' || c[i] == 'O')
 			c[i] = '0';
-		if (c[i] == 't' || c[i] == 'T')
+		else if (c[i] == 't' || c[i] == 'T')
 			c[i] = '7';
-		if (c[i] == 'l' || c[i] == 'L')
+		else if (c[i] == 'l' || c[i] == 'L')
 			c[i] = '1';
 	}
 	return ((char *)c);
