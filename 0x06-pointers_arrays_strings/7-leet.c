1cd87d80a056f3c8d674f37b50d72741ff13b61e
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-27 20:04:44 +0000

    betty7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+  *leet -  encodes a string into 1337.
+  *@c: - what to encode
+  *
+  *Return: char
+  */
+char *leet(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] == 'a' || c[i] == 'A')
+			c[i] = '4';
+		if (c[i] == 'e' || c[i] == 'E')
+			c[i] = '3';
+		if (c[i] == 'o' || c[i] == 'O')
+			c[i] = '0';
+		if (c[i] == 't' || c[i] == 'T')
+			c[i] = '7';
+		if (c[i] == 'l' || c[i] == 'L')
+			c[i] = '1';
+	}
+	return ((char *)c);
+}
+
