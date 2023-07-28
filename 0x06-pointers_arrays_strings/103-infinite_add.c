dd329f359ea3e29402242b7c0eab6fd3fd7a5232
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-28 03:06:06 +0000

    removed betty errors

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,15 @@
+#include "main.h"
+/**
+  *infinite_add -  adds two numbers
+  *@n1:ff
+  *@n2:ff
+  *@r:ff
+  *@size_r:rr
+  *
+  *Return:char
+  */
+
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len1 = strlen(n1);
@@ -7,9 +19,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int j = len2 - 1;
 	int k = 0;
 	int sum;
+	char temp;
+	int l;
+
 
 	if (len1 + len2 >= size_r)
-		return (0)i;
+		return (0);
 	while (i >= 0 || j >= 0 || carry)
 	{
 		sum = carry;
@@ -24,15 +39,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			j--;
 		}
 		carry = sum / 10;
-        r[k] = '0' + (sum % 10);
-        k++;
-    }
-	for (int l = 0, r_idx = k - 1; l < r_idx; l++, r_idx--) {
-          char temp = r[l];
-          r[l] = r[r_idx];
- r[r_idx] = temp;
+		r[k] = '0' + (sum % 10);
+		k++;
+	}
+	for (l = 0, r_idx = k - 1; l < r_idx; l++, r_idx--)
+	{
+		temp = r[l];
+		r[l] = r[r_idx];
+		r[r_idx] = temp;
 
 	}
-	r[k] = '\0'; // Null-terminate the result
-    return r;
+	r[k] = '\0';
+	return (r);
 }
