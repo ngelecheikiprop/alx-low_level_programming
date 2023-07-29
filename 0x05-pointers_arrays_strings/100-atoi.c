45b2de566a9c1e1e6f581b995eae276ebf9ea909
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-29 08:51:18 +0000

    i wont take so many numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,13 +11,17 @@ int _atoi(char *s)
 	int len;
 	int num = 0;
 	int sign = 1;
+	int numberExtracted = 0;
 
 	for (len = 0; s[len] != '\0'; len++)
 	{
 		if (s[len] >= '0' && s[len] <= '9')
 		{
 			num = num * 10 + (s[len] - '0');
+			numberExtracted = 1;
 		}
+		else if (numberExtracted && s[len] == ' ')
+			break;
 		if (s[len] == '-')
 			sign = sign * -1;
 	}
