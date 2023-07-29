e75bc44dce18e6ef984c1ab11e42c217ac804604
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-29 13:29:27 +0000

    generating passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  *main - entry point
+  *
+  *Return: Nothing
+  */
+int main(void)
+{
+	char charSet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(";
+	int options = sizeof(charSet) - 1;
+	int i;
+	int takeIndex;
+	char password[10];
+
+	srand(time(NULL));
+	for (i = 0; i < 10; i++)
+	{
+		takeIndex = rand() % options;
+		password[i] = charSet[takeIndex];
+	}
+	password[10] = '\0';
+	printf("Generated Password: %s\n", password);
+	return (0);
+}
