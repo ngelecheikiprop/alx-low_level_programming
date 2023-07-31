cbbd85619a583bfa8af16159d186bd5df53835fb
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-07-31 12:13:28 +0000

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_strchr -  locates a character in a string.
+  *@s: string s
+  @c:character c
+  *
+  *Return: char
+  */
+char *_strchr(char *s, char c)
+{
+	char* myString = s;
+	int i;
+
+	for (i = 0; *(myString + i) != c; i++)
+	{
+	}
+	return (s + i);
+}
