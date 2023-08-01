b7ed7b512dec331633e109c0717eef4174fcbdf4
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-01 06:31:00 +0000

    Double pointer, double fun

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -9,17 +9,5 @@
   */
 void set_string(char **s, char *to)
 {
-	int len;
-	int i;
-
-	if (s == ((void *)0))
-		return;
-	for (len = 0; to[len] != '\0'; len++)
-	{
-	}
-	*s = (char *)malloc((len + 1) * sizeof(char));
-	for (i = 0; i <= len; i++)
-	{
-		(*s)[i] = to[i];
-	}
+	*s = to;
 }
