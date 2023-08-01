b02afd6e00fc6b468dfe5693b51a1bd8571a9331
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-01 08:28:16 +0000

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,11 +11,11 @@ int _atoi(char *s)
 	int len;
 	int num = 0;
 	int sign = 1;
-	int numberExtracted = 0;
+	/*int numberExtracted = 0;*/
 
 	for (len = 0; s[len] != '\0'; len++)
 	{
-		if (s[len] >= '0' && s[len] <= '9')
+		/*if (s[len] >= '0' && s[len] <= '9')
 		{
 			num = num * 10 + (s[len] - '0');
 			numberExtracted = 1;
@@ -23,7 +23,17 @@ int _atoi(char *s)
 		else if (numberExtracted && s[len] == ' ')
 			break;
 		if (s[len] == '-')
-			sign = sign * -1;
+			sign = sign * -1;*/
+		if (s[len] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[len] >= '0' && s[len] <= '9')
+		{
+			num = num * 10 + (s[len] - '0');
+		}
+		else if (num > 0)
+			break;
 	}
 	return (num * sign);
 }
