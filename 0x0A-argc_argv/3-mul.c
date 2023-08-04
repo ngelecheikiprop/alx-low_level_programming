1e410cd2f1b4bc2cccb50eb91df191b3110e000a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-04 19:50:21 +0000

    finished number 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - the entry point
+  *@argc: argument count
+  *@argv: argument vector
+  *
+  *Return: Always 0
+  */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int mul;
+
+	if (argc <= 1)
+		printf("Error\n");
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
