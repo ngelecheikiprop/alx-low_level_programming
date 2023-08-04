bc622ca3efd86c553eecf6c8e3a31e0a944c2567
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-04 20:17:14 +0000

    finished number 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main -  adds two numbers
+  *@argc: the count of arguments given
+  *@argv: the vector of arguments as strings
+  *
+  *Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int number;
+
+	if (argc == 0)
+		return (0);
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] >= '0' && *argv[i] <= '9')
+		{
+			number = atoi(argv[i]);
+			sum += number;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
