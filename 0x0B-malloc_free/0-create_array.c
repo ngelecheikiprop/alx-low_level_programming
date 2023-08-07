9da0d5ed11fd5a4419f82743a9779e4be05c3b06
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-07 09:31:34 +0000

    finished number 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *create_array - makes an array of characters
+  *and intializes it with specific character
+  *@size:size of array
+  *@c:character to fill with
+  *
+  *Return: pointer to array
+  *NULL if size = 0;
+  *NULL if it fails
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		str = malloc(sizeof(char) * size);
+		if (str == NULL)
+		{
+			return (NULL)
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+			{
+				str[i] = 'H';
+			}
+			return (str);
+		}
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+Tasks on dynamic memory allocation
+
