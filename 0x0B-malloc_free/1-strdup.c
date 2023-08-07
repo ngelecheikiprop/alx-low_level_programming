63401d5e5e3e8d6c205f70dbf01ad08244645cd5
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-07 13:18:28 +0000

    finished n tested number 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  *_strdup - duplicates a string
+  *@str: string to duplicate
+  *
+  *Return: pointer to duplicate string
+  */
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *newstr;
+
+	if (str == NULL)
+		return (NULL);
+	len = strlen(str);
+	newstr = malloc(sizeof(char) * len);
+	for (i = 0; i < len; i++)
+	{
+		newstr[i] = str[i];
+	}
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (newstr);
+	}
+}
