7d48fd34bf6d72f545635902a455096756f22e2e
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-08 10:29:56 +0000

    check newstr if null

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,14 +9,16 @@
   */
 char *_strdup(char *str)
 {
-	int i;
-	int len;
+	unsigned int i;
+	unsigned int len;
 	char *newstr;
 
 	if (str == NULL)
 		return (NULL);
 	len = strlen(str);
 	newstr = malloc(sizeof(char) * (len + 1));
+	if (newstr == NULL)
+		return (NULL);
 	for (i = 0; i < len; i++)
 	{
 		newstr[i] = str[i];
