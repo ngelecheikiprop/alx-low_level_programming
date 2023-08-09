88b75b63a282019eaba9ef9f50dd56bfa792f6b1
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-09 02:32:36 +0000

    finished number 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  *argstostr - changes arguments to string
+  *@c: the number of arguments
+  *@av: vector of arguments to make string
+  *
+  *Return: one string with all arguments
+  */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *newstr;
+	int length = 0;
+
+	if ((ac == 0) || (av == NULL))
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		length  += strlen(av[i]);
+	}
+	length = length + ac + 2;
+	newstr = malloc(length * sizeof(char));
+	if (newstr == NULL)
+		return (NULL);
+	strcpy(newstr, av[0]);
+	strcat(newstr, "\n");
+	for (i = 1; i < ac; i++)
+	{
+		strcat(newstr, av[i]);
+		strcat(newstr, "\n");
+	}
+	return (newstr);
+}
