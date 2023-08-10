dfb8c8a676b43e3d29cc025b1f7eb169212ee3d7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-10 12:37:55 +0000

    added more features

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,6 +5,7 @@
   *string_nconcat - takes s1 and part or whole of s2
   *@s1: first string
   *@s2: second string
+  *@n:maximum bytes of s2
   *
   *Return: total string
   */
@@ -14,8 +15,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int lens1;
 	unsigned int nullbyte = 1;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	lens1 = strlen(s1);
 	newstr = malloc((lens1 + n + nullbyte) * sizeof(char));
+	if (newstr == NULL)
+		return (NULL);
 	strcpy(newstr, s1);
 	strncat(newstr, s2, n);
 	return (newstr);
