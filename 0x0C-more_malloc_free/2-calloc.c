7f6d0c4b1cb15727d6a039b6c3626c0cdf470ce9
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-08-10 16:19:08 +0000

    finished number 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  *_calloc - allocate continous memory
+  *@nmemb:number of members
+  *@size:size of each member
+  *
+  *Return: pointer to location
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void **ptr = malloc(sizeof(void *) * nmemb);
+	unsigned int i;
+	unsigned int j;
+
+	if ((nmemb == 0) || (size == 0))
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = malloc(sizeof(char) * size);
+		if (ptr == NULL)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				free(ptr[j]);
+				return (NULL);
+			}
+		}
+	}
+	memset(ptr, 0, (size * nmemb));
+	return (ptr);
+}
