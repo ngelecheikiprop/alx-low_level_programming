6b13d5e51684bad4d9aa79ea4ba41007e7b5ec71
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-14 13:40:36 +0000

    checked first d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
+#include <stdlib.h>
 #include "dog.h"
 #include <string.h>
-#include <stdlib.h>
 /**
   * init_dog - intializes the structure of type dog
   *@d: pointer to structure
@@ -12,13 +12,9 @@
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = malloc(sizeof(char) * (strlen(name) + 1));
-	if (d->name == NULL)
-		printf("cannot allocate memory for name");
-	if (d->owner == NULL)
-		printf("cannot allocate memory for owner");
-	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	strcpy(d->name, name);
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
 	d->age = age;
-	strcpy(d->owner, owner);
+	d->owner = owner;
 }
