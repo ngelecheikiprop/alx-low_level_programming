ca999dc3ca03b53a0799d9975ea2905bb235d37f
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-14 19:44:42 +0000

    got the location of the structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdio.h>
+#include "dog.h"
 /**
   *print_dog - prints a struct dog
   *@d: pointer to structure
@@ -9,19 +10,16 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
-		printf("");
-		exit();
-	}
-	if (d->name == NULL)
+		return;
+	if (&(d->name) == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age == NULL)
+	if (&(d->age) == NULL)
 		printf("Age: (nil)\n");
 	else
-		prinft("Age: %d\n", d->age);
-	if (d->owner == NULL)
+		printf("Age: %f\n", d->age);
+	if (&(d->owner) == NULL)
 		printf("Owner: (nill)\n");
 	else
 		printf("Owner: %s\n", d->owner);
