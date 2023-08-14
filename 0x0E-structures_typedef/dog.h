6eac5d2f6ddaa3c6bc972508fc3ad496377590a9
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-14 09:13:32 +0000

    removed betty errors on number 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef STRUCT
 #define STRUCT
-
+/**
+  *struct dog - creating a dog structure
+  *@name: name of the doge
+  *@age: age of dong
+  *@owner: name of owner
+  *
+  *Description: nothing to add
+  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
 
 #endif
