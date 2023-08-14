415bd2a1a962072d12325a44dbe1b7b304268e66
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-14 21:13:59 +0000

    added the library needed number 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - prints the file it was compiled from
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
