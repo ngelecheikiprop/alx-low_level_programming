c1c8233dc425130ac24f56ce8c70562f9b3d8813
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-15 07:19:38 +0000

    redid number 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,15 +13,15 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if (&(d->name) == NULL)
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (&(d->age) == NULL)
-		printf("Age: (nil)\n");
+	if (d->age < 0)
+		printf("Name: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
-	if (&(d->owner) == NULL)
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
