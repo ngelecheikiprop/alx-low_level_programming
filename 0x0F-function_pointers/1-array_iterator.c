0cc9ab7754368899f8e225058cf5935fa8c5cf22
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-16 17:55:43 +0000

    finished number 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+/**
+ * array_iterator - goes through array impleenting
+ * a function
+ * @array: the array to pass through
+ * @size: the size of the array
+ * @action: what to do to each element
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for(i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
