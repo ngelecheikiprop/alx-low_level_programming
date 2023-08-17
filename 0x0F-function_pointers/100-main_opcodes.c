4fafb1742d9ef79b13f88d7bb5f4dd9a87a4bc08
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-17 02:14:11 +0000

    removed betty errors

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * print_opcodes - prints the opcodes
+ * @bytes: bytes given by the user
+ *
+ * Return: nothing
+ */
+void print_opcodes(int bytes)
+{
+	unsigned char *ptr = (unsigned char *)print_opcodes;
+	int i;
 
-void print_opcodes(int bytes) {
-    unsigned char *ptr = (unsigned char *)print_opcodes;
-    int i;
-    
-    for (i = 0; i < bytes; i++) {
-        printf("%02x ", *ptr);
-        ptr++;
-    }
-    printf("\n");
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x ", *ptr);
+		ptr++;
+	}
+	printf("\n");
 }
+/**
+ * main - entry point
+ * @argc: count of arguments
+ * @argv:the vector having arguments
+ *
+ * Return: a number
+ */
+int main(int argc, char *argv[])
+{
+	int bytes;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
-    
-    int bytes = atoi(argv[1]);
-    
-    if (bytes <= 0) {
-        printf("Error\n");
-        return 2;
-    }
-    
-    print_opcodes(bytes);
-    
-    return 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes <= 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	print_opcodes(bytes);
+	return (0);
 }
 
