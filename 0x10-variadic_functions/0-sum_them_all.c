0009aa50600bf1668e19bcc09eac59e3c255f198
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-17 12:59:57 +0000

    can compile with flags

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,9 +8,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int sum = 0, i;
+	int sum = 0;
+	unsigned int i;
 
-	if ((n == 0) || (n < 0))
+	if (n == 0)
 		return (0);
 	va_start(ap, n);
 
