4eaf6db2bec3af7a69beadd4c475652896959d95
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-18 04:58:21 +0000

    adding new line when failed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(numbers, n);
 
 	printf("%d", va_arg(numbers, int));
