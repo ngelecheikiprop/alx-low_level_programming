c0f4d98614c45b84a8ba1cac738b99e08ca74c91
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-24 21:16:31 +0000

    removed betty error 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
 /**
  * print_list - prints all the elements of a list
  *
- * @list_t: the pointer to the list
+ * @h: the pointer to the list
  *
  * Return: the number of nodes
  */
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 	{
 		count++;
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0,"(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
