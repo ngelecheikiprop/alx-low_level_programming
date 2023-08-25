c3e03d7b494549028f2d93ec8835df756575d566
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-25 07:07:39 +0000

    removed betty erros for number 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 #include <stddef.h>
-
+/**
+  *list_len - says the number of elemts in a list
+  *@h: the head of the list
+  *Return: the number of elements in the list
+  */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
