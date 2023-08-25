7d9899207bd205f6e20f811915317d3236b5dc36
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-25 09:26:57 +0000

    done number 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+  *first - runs before main
+  *
+  *Return: nothing
+  */
+void first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
