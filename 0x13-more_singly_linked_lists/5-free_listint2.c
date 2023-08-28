b42f8c90bb3185f0c91968e4af50df3cc184f784
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-28 15:56:11 +0000

    uploaded number 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  *free_listint2 - free a list and its head to NULL
+  *@head: the begining of the list to free
+  *
+  *Return: nothing
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	listint_t *temp;
+
+	current = *head;
+	while (current != NULL)
+	{
+		temp = current->next;
+		free(current);
+		current = temp;
+	}
+	*head = NULL;
+}
