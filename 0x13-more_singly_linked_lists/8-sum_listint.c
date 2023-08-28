a2e2bfd5cbdea100a79dbc6d878a88cca20184a6
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-28 11:57:53 +0000

    done number 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  *sum_listint - adds everything in the list
+  *@head: the start of the list
+  *
+  *Return: the sum of items in the list
+  */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while(head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
