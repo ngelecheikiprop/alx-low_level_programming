bb5af0c9d46007d33aae9ec16171cb8f482d72be
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-29 06:59:23 +0000

    removed betty errors number 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,13 +2,14 @@
 /**
   *print_listint - lists elements in a
   *linked list
-  *@listint_t: this is the pointer to the list
+  *@h: this is the pointer to the list
   *
   *Return: the number of nodes
   */
 size_t print_listint(const listint_t *h)
 {
 	size_t no_of_nodes = 0;
+
 	while (h != NULL)
 	{
 		no_of_nodes++;
