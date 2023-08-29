4970e84754bbae36c3ff1ad54013c745eb9b5106
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-29 17:32:57 +0000

    removed betty errors number 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		while (count < idx)
 		{
 			count++;
-			if(current->next != NULL)
+			if (current->next != NULL)
 				current = current->next;
 			else
 			{
