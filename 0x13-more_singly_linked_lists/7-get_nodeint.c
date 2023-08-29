6466d8680d5ad272605e274346df64b86ec1c9f0
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-29 10:51:09 +0000

    removed betty errors in number 7wq

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-  * get_nodeint_at_index - gets the node at the index given 
+  *get_nodeint_at_index - gets the node at the index given
   *@head: the start of the list given
   *@index: the index you need node at
   *
@@ -10,6 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 	listint_t *current = head;
+
 	while (count < index)
 	{
 		count++;
