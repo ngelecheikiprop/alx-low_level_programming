e98e70a9027d35c2114595e70b64a329caa17888
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-30 12:51:01 +0000

    done number 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int (*fun)(int, int);
+	int num1, num2;
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	fun = get_op_func(operator);
+	if (fun == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (num2 == 0 && ((strcmp(operator, "/") == 0) || strcmp(operator, "%") == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", fun(num1, num2));
+
+	return (0);
+}
