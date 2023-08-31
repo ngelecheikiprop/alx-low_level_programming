22899a0c57556b9a8e4b0d41d7626b8b85cd9dca
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-31 01:14:21 +0000

    sketch number 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+  *free_listint_safe - safely frees listint
+  *@h: the begining of the list
+  *
+  *Return: the size of the list it freed
+  */
+size_t free_listint_safe(listint_t **h)
+{
+	int count;
+	count++;
+	return (count);
+}
