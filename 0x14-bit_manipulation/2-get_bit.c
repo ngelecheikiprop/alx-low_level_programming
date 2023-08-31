64604b37610150497de1ef0f758a525e441fa0ea
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-08-31 18:53:40 +0000

    done number 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+  *get_bit - gets a bit
+  *@index: where
+  *@n: number
+  *
+  *Return: int
+  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
@@ -9,5 +15,5 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 	}
 	mask = 1UL << index;
-	return (n & mask) ? 1 : 0;
+	return ((n & mask) ? 1 : 0);
 }
