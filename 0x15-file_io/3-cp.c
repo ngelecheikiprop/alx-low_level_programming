b996c1e7f884dc93be2498ab9fdbc81eef6b491d
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-09-04 21:01:38 +0000

    done number 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+  *main - this is the entry point of
+  *my code
+  *@ac: the argument count
+  *@av: where the arguments are stored
+  *
+  *Return: nothing. always returns nothing
+  */
+int main(int ac, char *av[])
+{
+	int fdr;
+	int fdw;
+	ssize_t byte_read, byte_wrote;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fdr = open(av[1], O_RDONLY);
+	byte_read = read(fdr, buffer, 1024);
+	if (fdr == -1 || byte_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
+		exit(98);
+	}
+	fdw = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	byte_wrote = write(fdw, buffer, byte_read);
+	if (fdw == -1 || byte_wrote == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	return (0);
+}
