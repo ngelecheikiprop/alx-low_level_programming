aacdf2f4be5272f1973bee6b4215abf8c5a714e4
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-09-30 23:06:16 +0000

    worked on when head is null

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,13 +8,15 @@
   */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow = head->next;
-	listint_t *fast = head->next->next;
+	listint_t *slow;
+	listint_t *fast;
 
-	if (head == NULL || head->next == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
+	slow = head->next;
+	fast = head->next->next;
 	while (fast && fast->next)
 	{
 		if (slow == fast)
