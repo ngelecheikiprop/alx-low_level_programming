01249e90205aa89742fad5f55d9c4a5918e4b260
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-12-21 19:21:23 +0300

    counting elements
    Added a function to count elements in doubly linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,29 +1,19 @@
 #include "lists.h"
-
 /**
- * dlistint_len - returns the number of elements in
- * a double linked list
- *
- * @h: head of the list
- * Return: the number of nodes
+ *dlistint_len - prins values in a doubly linked list
+ *@h:the head of the list
+ *Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
+	size_t i = 0;
 
 	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+		return (0);
+	while (h)
 	{
-		count++;
 		h = h->next;
+		i++;
 	}
-
-	return (count);
+	return (i);
 }
