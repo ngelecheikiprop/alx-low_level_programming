b133dcf9e337e3d75899d8c58094c9be52e292eb
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-12-21 19:11:16 +0300

    printing lists
    This function is used for printing doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,30 +1,20 @@
 #include "lists.h"
-
 /**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
- *
- * @h: head of the list
- * Return: the number of nodes
+ *print_dlistint - prins values in a doubly linked list
+ *@h:the head of the list
+ *Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
+	size_t i = 0;
 
 	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+		return (0);
+	while (h)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		i++;
 	}
-
-	return (count);
+	return (i);
 }
