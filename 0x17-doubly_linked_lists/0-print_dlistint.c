a25516bee63122d77f9c3b3c56341d23d45af468
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-01-25 20:11:12 +0300

    remove betty errors

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,17 @@
 #include "lists.h"
-
+/**
+ * print_dlistint-prints a list
+ *
+ * @h: the head of lst
+ * Return: the number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t no_of_nodes = 0;
 
 	if (h == NULL)
 	{
-		return no_of_nodes;
+		return (no_of_nodes);
 	}
 	while (h)
 	{
