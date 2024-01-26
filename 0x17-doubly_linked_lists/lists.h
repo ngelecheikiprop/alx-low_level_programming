4f7126466ea5cb811cd58c2ae8bc0bbeed5ccafd
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-01-26 17:21:48 +0300

    this is mine

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,9 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef MAIN_H
+#define MAIN_H
 
+/*standard libraries*/
+#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -11,7 +12,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for ALX project
+ *
  */
 typedef struct dlistint_s
 {
@@ -20,15 +21,8 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+/*function prototypes*/
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
 
 #endif
