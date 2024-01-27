7229f6ad671467abf0c24d1d3ce33479b2197998
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-01-27 04:04:53 +0300

    added functionality of idx is equal to zero

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,6 +18,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (idx == 0)
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
 	current = *h;
 	while ((current) && (count < idx))
 	{
